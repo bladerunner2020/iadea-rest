4cb3fdacfd8b434f78dd9e546b44e717c10c24ea
bladerunner2020 <pivovarov@gmail.com>
2017-02-01 19:12:47 +0300

    fixed bug in Upload that fails if name of file contains not ASCII symbols (e.g. Russian)

--- iadea.js
+++ iadea.js
@@ -80,6 +80,23 @@ var connect = function(host, port, user, password) {
 };
 
 /**
+ * 
+ * @param str {String} 
+ * @returns {Number} returns the byte length of an utf8 string
+ */
+function byteLength(str) {
+    var s = str.length;
+    for (var i=str.length-1; i>=0; i--) {
+        var code = str.charCodeAt(i);
+        if (code > 0x7f && code <= 0x7ff) s++;
+        else if (code > 0x7ff && code <= 0xffff) s+=2;
+        if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
+    }
+    return s;
+}
+
+
+/**
  * Upload a file to device
  * @public
  * @param {String} filename full path to file to upload
@@ -186,8 +203,8 @@ var uploadFile = function (filename, downloadPath) {
 
     var formEnd = '\r\n--' + boundaryKey + '--';
 
-    var contentLength = formStart.length + formEnd.length + fileSize;
-    req.setHeader('Content-Length',contentLength);
+    var contentLength = byteLength(formStart) + byteLength(formEnd) + fileSize;
+    req.setHeader('Content-Length', contentLength);
 
     req.write(formStart);
     
